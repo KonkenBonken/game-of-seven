60b4865d2cdcda26f79053ab827553f4f605a6de
KonkenBonken <konrad05pettersson@gmail.com>
2023-03-05 22:29:13 +0100

    Set start angle to straight up instead of right

--- src/Aim.tsx
+++ src/Aim.tsx
@@ -1,11 +1,11 @@
 import useMouse from '@react-hook/mouse-position';
 
-let lastAngle = 0;
+let lastAngle = Math.PI * 1.5;
 
 export function Aim() {
   const mouse = useMouse(document.body),
     angle = (!mouse.y || !mouse.x) ? lastAngle :
-      Math.atan2(mouse.y - window.innerHeight / 2, mouse.x - window.innerWidth / 2);
+      Math.atan2(mouse.y - window.innerHeight / 2, mouse.x - window.innerWidth / 2) + Math.PI * 2;
 
   lastAngle = angle;
 
